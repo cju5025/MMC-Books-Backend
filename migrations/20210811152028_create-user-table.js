9fcc75035556ee512df9517eb2796f0de303d914
Colter Ulrich <cju5025@gmail.com>
2021-08-11 15:27:57 -0600

    adds user table

---
+++ migrations/20210811152028_create-user-table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("user", user => {
+    user.increments()
+    user.string('username')
+    user.string('email')
+    user.string('password')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+};
