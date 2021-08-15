219510e048094075f6ce9559989a640a042c1910
Colter Ulrich <cju5025@gmail.com>
2021-08-15 13:09:41 -0600

    adds authorization upon sign in

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ const jwt = require('jsonwebtoken');
 const { Model } = require('objection');
 Model.knex(database);
 
+require('dotenv').config();
+
 const User = require('./Models/User');
 
 app.get('/', ( request, response ) => {
-    response.json({"message": "serving"})
+    response.json({"message": "serving" })
 })
 
 app.post('/users', ( request, response ) => {
-    console.log(request.body)
     const { user } = request.body
     bcrypt.hash(user.password, 12)
         .then(hashedPassword => {
@@ -38,6 +39,36 @@ app.post('/users', ( request, response ) => {
         .then(users => {
             const user = users[0]
             response.json({ user })
+        }).catch(error => response.json(error.message))
+})
+
+app.post('/login', ( request, response ) => {
+    const { user } = request.body
+
+    database('user')
+        .select()
+        .where({ email: user.email })
+        .first()
+        .then(retrievedUser => {
+            if (!retrievedUser) throw new Error ('Incorrect email or password')
+
+            return Promise.all([
+                bcrypt.compare(user.password, retrievedUser.password),
+                Promise.resolve(retrievedUser)
+            ])
+        }).then(results => {
+            const arePasswordsTheSame = results[0]
+            const user = results[1]
+
+            if (!arePasswordsTheSame) throw new Error('Incorrect email or password')
+
+            const payload = { email: user.email }
+            const secret = process.env.JWT_SECRET 
+
+            jwt.sign(payload, secret, (error, token) => {
+                if (error) throw new Error("Signing didn't work")
+                response.json({ user, token })
+            })
         }).catch(error => response.json(error.message))
 })
 
